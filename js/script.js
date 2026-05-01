78545a2af5e08406117ba443c00ddecdc46b3a26
happle joseph <happlejoseph66@gmail.com>
2026-05-01 12:23:40 +0530

    improve the ui

--- js/script.js
+++ js/script.js
@@ -2,3 +2,29 @@
 
 let display = document.getElementById('display');
 let buttons = document.querySelectorAll('button');
+
+buttons.forEach(button=> {
+    button.addEventListener('click', ()=> {
+        let value = button.innerText;
+
+        if (value === 'C') {
+            display.value = '';
+        }
+        else if (value === '=') {
+            try {
+                display.value = eval(display.value);
+            } catch (error) {
+                display.value = 'Error';
+            }
+        }
+        else if (value === '←') {
+            display.value = display.value.slice(0, -1);
+
+        }
+        else {
+            display.value += value;
+        }
+
+    })
+
+})
